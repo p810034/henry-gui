0e39b6bddf3184a9179c7a3ac947ab2019ab0895
p810034 <henryxyzxyz@gmail.com>
2021-03-04 11:39:00 +0800

    參考 karthick3018/react-gui adding to repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './components/main' 
+import Sidebar from './components/sidebar' 
+
+function App() {
+  return (
+   <div>
+     <Main/>
+     {/* <Sidebar/> */}
+   </div>
+  );
+}
+
+export default App;
