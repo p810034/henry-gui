c51e73f604a3ecbe7c271235aef34a24162c3215
p810034 <henryxyzxyz@gmail.com>
2021-03-08 17:58:27 +0800

    formatting

--- src/helpers/generateUiElements.js
+++ src/helpers/generateUiElements.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 export const returnRespectiveHtmlElement = (type) => {
-  switch (type){
-    case 0:
-      return <button>I'm button</button>
-    case 1:
-      return <input defaultValue="i'm input"/>
-     case 2:
-      return <textarea defaultValue="i'm text area"/> 
-     default:
-      return <></> 
-  }
+    switch (type) {
+        case 0:
+            return <button>I'm button</button>
+        case 1:
+            return <input defaultValue="i'm input" />
+        case 2:
+            return <textarea defaultValue="i'm text area" />
+        default:
+            return <></>
+    }
 }
 
-export const generatedElements = (type,id) => {
-  const htmlElement = returnRespectiveHtmlElement(type);
-  const uiElementArray=[];
+export const generatedElements = (type, id) => {
+    const htmlElement = returnRespectiveHtmlElement(type);
+    const uiElementArray = [];
 
-  uiElementArray.push({
-    id: `${200+id}`,
-    content: htmlElement
-  }) 
-  return uiElementArray;
+    uiElementArray.push({
+        id: `${200 + id}`,
+        content: htmlElement
+    })
+    return uiElementArray;
 }
