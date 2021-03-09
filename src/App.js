973a8d0c38ba79c444f83d846f060f1c5ec64ef0
p810034 <henryxyzxyz@gmail.com>
2021-03-09 17:59:37 +0800

    additonal sytles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Main from './components/renderPlace/index'
-import './App.css'
 
 function App() {
   return (
--- src/helpers/styleFn.js
+++ src/helpers/styleFn.js
@@ -2,33 +2,25 @@
 const grid = 8
 
 export const getSidebarListStyle = isDraggingOver => ({
-    background: isDraggingOver ? 'lightblue' : '#2E3648',
+    background: '#2E3648',
     padding: grid,
     width: 250
 });
 
 export const getSidebarItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
     userSelect: 'none',
     padding: grid * 2,
     margin: `0 0 ${grid}px 0`,
+    color: isDragging?'grey':'white',
+    background: isDragging && '#B2F5EA',
 
-    // change background colour if dragging
-    background: isDragging ? 'lightgreen' : 'grey',
-
-    // styles we need to apply on draggables
     ...draggableStyle
 });
 
 export const getDraggableItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
     userSelect: 'none',
     padding: grid * 2,
     margin: `0 0 ${grid}px 0`,
-
-    // change background colour if dragging
     background: isDragging && 'lightgreen' ,
-
-    // styles we need to apply on draggables
     ...draggableStyle
 });
